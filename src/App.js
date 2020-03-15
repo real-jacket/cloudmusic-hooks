c5283dd2220d99b5c2fca226c31f45263269c478
jacket <1762982273@qq.com>
2020-03-15 15:34:54 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { GlobalStyle } from './style'
+import { IconStyle } from './assets/iconfont/iconfont'
+
+function App() {
+  return (
+		<div className='App'>
+			<GlobalStyle></GlobalStyle>
+			<IconStyle></IconStyle>
+			<i className='iconfont'>&#xe62b;</i>
+		</div>
+  )
+}
+
+export default App

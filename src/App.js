6b57730fbf9760b48549055a2b01fc9f4c6eb961
jacket <1762982273@qq.com>
2020-03-15 18:14:18 +0800

    redux init

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import { renderRoutes } from 'react-router-config'
 import { IconStyle } from './assets/iconfont/iconfont'
 import routes from './routes'
 import { HashRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 
 function App() {
 	return (
-		<HashRouter>
-			<GlobalStyle></GlobalStyle>
-			<IconStyle></IconStyle>
-			{renderRoutes(routes)}
-		</HashRouter>
+		<Provider store={store}>
+			<HashRouter>
+				<GlobalStyle></GlobalStyle>
+				<IconStyle></IconStyle>
+				{renderRoutes(routes)}
+			</HashRouter>
+		</Provider>
 	)
 }
 
---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { compose, createStore, applyMiddleware } from "redux";
+import reducer from "./reducer";
+import thunk from "redux-thunk";
+
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTEND_COMPOSE__ || compose
+
+const store = createStore(reducer, composeEnhancers(
+    applyMiddleware(thunk)
+))
+
+export default store

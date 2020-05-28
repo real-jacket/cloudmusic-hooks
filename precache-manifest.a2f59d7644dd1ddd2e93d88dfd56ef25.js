36b42ec3506dea5b406ad3a6d6765e3cd979eb20
jacket <1762982273@qq.com>
2020-05-28 19:52:53 +0800

    Updates

---
+++ precache-manifest.a2f59d7644dd1ddd2e93d88dfd56ef25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e0d3fb12cf25ffc030c74fe716c7daf",
+    "url": "/cloudmusic-hooks/index.html"
+  },
+  {
+    "revision": "adfc1955cd7521860129",
+    "url": "/cloudmusic-hooks/static/css/2.0988ea1f.chunk.css"
+  },
+  {
+    "revision": "adfc1955cd7521860129",
+    "url": "/cloudmusic-hooks/static/js/2.a6c31c18.chunk.js"
+  },
+  {
+    "revision": "aad9b26a3b91a0b3c9542f8599c56ee6",
+    "url": "/cloudmusic-hooks/static/js/2.a6c31c18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d91603a3819456cf531",
+    "url": "/cloudmusic-hooks/static/js/main.0624f79d.chunk.js"
+  },
+  {
+    "revision": "804a32a2a58833a5eec0",
+    "url": "/cloudmusic-hooks/static/js/runtime-main.96d9038c.js"
+  },
+  {
+    "revision": "64444b39f93291cc186672eca04e6a7f",
+    "url": "/cloudmusic-hooks/static/media/music.64444b39.png"
+  }
+]);

7c0f938054b86c85792cbe8582f50b18ecdc80cb
jacket <1762982273@qq.com>
2020-07-17 16:33:53 +0800

    fix github-aciton and react nev config

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,17 +1,16 @@
-import axios from 'axios';
+import axios from 'axios'
+
+const baseURL =
+  process.env.NODE_ENV === 'production' ? process.env.REACT_APP_BASE_API : 'http://localhost:3000'
 
 // axios 拦截器
-const axiosInstance = axios.create({
-  // baseURL: process.env.REACT_APP_BASE_API
-  baseURL:'http://localhost:3000'
-})
+const axiosInstance = axios.create({ baseURL })
 
 axiosInstance.interceptors.response.use(
-  res=> res.data,
-  err =>{
-    console.log(err,"网络错误")
-  }
+  (res) => res.data,
+  (err) => {
+    console.log(err, '网络错误')
+  },
 )
-
 
 export default axiosInstance
